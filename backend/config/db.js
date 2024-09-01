9d5ad13e1af7bbd87121df4992fd1109a59d93e1
ashutosh229 <akumarjha875@gmail.com>
2024-09-01 15:00:07 +0530

    issue 1 solved with comments

--- backend/config/db.js
+++ backend/config/db.js
@@ -12,6 +12,3 @@ const connectDB = async () => {
 };
 
 export default connectDB;
-
-//push
-//
--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,16 @@
+//importing all the necessary dependencies
 import express from "express";
 import connectDB from "./config/db.js";
 import router from "./routes/router.js";
 
+//dotenv configurations
 import dotenv from "dotenv";
 dotenv.config({});
 
+//this is the application
 const app = express();
 
-//port
+//this is the port number
 const port = 3000;
 
 //apis
