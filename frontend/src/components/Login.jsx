a9184df863f980ea2149334605820e89ed6f8a6d
Slok9931 <148342312+Slok9931@users.noreply.github.com>
2024-09-01 16:55:38 +0530

    djfa

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -5,7 +5,7 @@ const Login = () => {
     <div className="container">
       <div className="login-form">
         <h1>CCPS</h1>
-        <p className="center">Get jobs here.</p>
+        <p className="first">Centre for Career Planning ans Services </p>
         <form>
           <div className="form-group">
             <label htmlFor="email">Email</label>
@@ -22,7 +22,11 @@ const Login = () => {
         </p>
         <p>Do not have an account yet? <a href="#">Sign Up</a></p>
       </div>
+      <div className="image">
+        <img src="../src/assets/bubble.jpeg" alt="" />
+      </div>
     </div>
+
 
   )
 }
